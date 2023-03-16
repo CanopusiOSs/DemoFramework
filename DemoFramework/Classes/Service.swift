00ed43b25647a8031831f1ccfe539757c45e8249
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-16 18:15:24 +0530

    Initial commit

---
+++ DemoFramework/Classes/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  DemoFramework
+//
+//  Created by IOS on 16/03/23.
+//
+
+import Foundation
+
+public class Service {
+    public init() {}
+    public func sayHello() -> String {
+        return "Hello to you"
+    }
+}
